c43917f1a935028f0544eec8b106ccd8cbec1bbf
Skuznetz <s.kuznetzov@mail.ru>
2018-11-02 19:54:33 +0300

    напишем компонент Game

---
+++ src/containers/GamePage.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default class Game extends Component {
+    render() {
+        return (
+            <div>Hello</div>
+        );
+    }
+}

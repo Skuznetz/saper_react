be51cc618041866a20c60538aee086662c543a99
Skuznetz <s.kuznetzov@mail.ru>
2018-11-02 20:20:33 +0300

    импорт в main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route, Redirect, browserHistory } from 'react-router';
 
+import App from './components/App.jsx';
+import GamePage from './containers/GamePage.jsx';
+import StartPage from './components/StartPage.jsx';
+
+import store from './store';
 
 import 'normalize.css';
 import './assets/main.css';

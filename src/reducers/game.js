38c85f7586c82939d29513b45fde71a8873bcbf0
Skuznetz <s.kuznetzov@mail.ru>
2018-11-03 10:04:17 +0300

    изменим редюсер

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -4,3 +4,16 @@ import { startGame,defaultGameState } from '../utils/minesweeper';
 import {
     START_GAME
 } from '../actions';
+
+export default (state = DEFAULT_STATE, action) => {
+    switch (action.type) {
+        case START_GAME: {
+            const { rows, cols, mines } = action;
+
+            return startGame({ rows, cols, mines });
+        }
+             default: {
+            return state;
+        }
+    }
+};

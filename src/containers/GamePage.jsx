81cce2acaffeebc057dc9c0aa14c6db255204528
Skuznetz <s.kuznetzov@mail.ru>
2018-11-03 09:55:03 +0300

    пишем connect

--- src/containers/GamePage.jsx
+++ src/containers/GamePage.jsx
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
+@connect(
+    null,
+    actions
+)
 export default class Game extends Component {
     render() {
         return (

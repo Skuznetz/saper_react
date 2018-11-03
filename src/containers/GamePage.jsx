7236df3e68077198d64a442a484f1a2d6220a0d3
Skuznetz <s.kuznetzov@mail.ru>
2018-11-03 09:54:13 +0300

    импортируем экшены

--- src/containers/GamePage.jsx
+++ src/containers/GamePage.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 export default class Game extends Component {
     render() {

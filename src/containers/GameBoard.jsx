0ed971ba502f0e37976561adf256f1133fc385cc
Skuznetz <s.kuznetzov@mail.ru>
2018-11-05 18:46:42 +0300

    импортируем Board

--- src/containers/GameBoard.jsx
+++ src/containers/GameBoard.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
+import Board from '../components/Board.jsx';
 
 @connect(
     mapStateToProps,

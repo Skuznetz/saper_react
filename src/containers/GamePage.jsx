5dfebe9ebf558c33adae9dbbd060162be5721b91
Skuznetz <s.kuznetzov@mail.ru>
2018-11-05 18:59:18 +0300

    импортируем GameBoard

--- src/containers/GamePage.jsx
+++ src/containers/GamePage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
+import GameBoard from './GameBoard.jsx';
 
 @connect(
     null,

0784ffbfb1b99912e30d6e4530b12ed314f52c6b
Skuznetz <s.kuznetzov@mail.ru>
2018-11-05 18:48:16 +0300

    функция получения сетки

--- src/containers/GameBoard.jsx
+++ src/containers/GameBoard.jsx
@@ -20,5 +20,19 @@ export default class GameBoard extends Component {
     }
 }
 
+function getGameBoard(state) {
+    const board = state.getIn(['game', 'board']);
+    const cols = state.getIn(['game', 'cols']);
+
+    return board.reduce((rows, cell) => {
+        const rowIdx = Math.floor(cell.get('id') / cols);
+        const row = rows.get(rowIdx);
+
+        return row
+            ? rows.set(rowIdx, row.push(cell))
+            : rows.push(new List([cell]));
+    }, new List());
+}
+
 
 

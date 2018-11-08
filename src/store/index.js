0a8ebb1636899c6e6cdbae689ddb4234704ccafb
Skuznetz <s.kuznetzov@mail.ru>
2018-11-08 17:06:29 +0300

    перекинем на index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 
 import thunk from 'redux-thunk';
 
-import rootReducer from '../reducers/game.js';
+import rootReducer from '../reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 

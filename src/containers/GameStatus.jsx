32713e812ac5f928dcf5c92dbf457de6b06767b2
Skuznetz <s.kuznetzov@mail.ru>
2018-11-11 08:19:15 +0300

    class GameStatus

--- src/containers/GameStatus.jsx
+++ src/containers/GameStatus.jsx
@@ -5,6 +5,14 @@ import EmojiStatus from '../components/EmojiStatus.jsx';
 
 @connect(mapStateToProps)
 
+export default class GameStatus extends Component {
+    render() {
+        const { status } = this.props;
+
+        return <EmojiStatus mapper={statusEmojis} status={this.props.status} />;
+    }
+}
+
 function mapStateToProps(state, ownProps) {
     return {
         status: getGameStatus(state)

228656173c560d0f57e1eafc07848c5a23adada7
Skuznetz <s.kuznetzov@mail.ru>
2018-11-11 08:18:04 +0300

    mapStateToProps

--- src/containers/GameStatus.jsx
+++ src/containers/GameStatus.jsx
@@ -4,3 +4,9 @@ import { connect } from 'react-redux';
 import EmojiStatus from '../components/EmojiStatus.jsx';
 
 @connect(mapStateToProps)
+
+function mapStateToProps(state, ownProps) {
+    return {
+        status: getGameStatus(state)
+    };
+}
